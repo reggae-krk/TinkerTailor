09d519b3e6b24152aee69ff58f5e13e7655e99b5
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-11 17:10:35 +0200

    Create constructor and attributes

--- src/main/java/TinkerTailor.java
+++ src/main/java/TinkerTailor.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TinkerTailor {
+
+    private List<Integer> list;
+    private final int step;
+    private int size;
+
+    public TinkerTailor(int size, int step) {
+        this.list = new ArrayList<Integer>();
+        this.size = size;
+        this.step = step;
+        fillList();
+    }
 }
