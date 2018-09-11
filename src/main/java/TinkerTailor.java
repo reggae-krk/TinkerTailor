c455dae1587c3cdb2fdeaa95b999f38da12ed349
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-11 15:41:10 +0200

    Revert "Create constructor and attributes"
    This reverts commit b07b21a

--- src/main/java/TinkerTailor.java
+++ src/main/java/TinkerTailor.java
@@ -1,12 +1,2 @@
 public class TinkerTailor {
-
-    private int[] array;
-    private int size;
-    private int step;
-
-    public TinkerTailor(int[] array, int size, int step) {
-        this.array = array;
-        this.size = size;
-        this.step = step;
-    }
 }
